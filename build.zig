5b8b3516e7d22f739e48712036d7fb67600c4a1f
Parzival-3141 <29632054+Parzival-3141@users.noreply.github.com>
2023-01-11 06:34:57 -0500

    included freetype, working on font altas

--- build.zig
+++ build.zig
@@ -1,9 +1,7 @@
 const std = @import("std");
+const Builder = std.build.Builder;
 
-const includes = "deps/include/";
-const libs = "deps/lib/";
-
-pub fn build(b: *std.build.Builder) void {
+pub fn build(b: *Builder) void {
     const target = b.standardTargetOptions(.{});
     const mode = b.standardReleaseOptions();
 
@@ -16,22 +14,10 @@ pub fn build(b: *std.build.Builder) void {
     const csources = get_c_sourcefiles(b, "src") catch unreachable;
     defer csources.deinit();
 
-    std.debug.print("Compiling source files: {s}\n", .{csources.items});
-    exe.addCSourceFiles(csources.items, &.{ "-Wall", "-Wextra", "-pedantic", "-g" });
+    exe.addCSourceFiles(csources.items, &.{ "-Wall", "-Wextra", "-pedantic", "--debug", "-Wno-unused-parameter" });
 
-    includeLibrary(exe, "SDL2");
-    b.installBinFile(libs ++ "SDL2/SDL2.dll", "SDL2.dll");
-
-    includeLibrary(exe, "SDL2_ttf");
-    b.installBinFile(libs ++ "SDL2_ttf/SDL2_ttf.dll", "SDL2_ttf.dll");
-
-    // Can't statically link unless I compile with zig cc/c++, because
-    // for some reason you can't mix static libs from different compilers >:(
-    //exe.addIncludePath(includes ++ "freetype");
-    //exe.addObjectFile(libs ++ "freetype/freetype.lib");
-
-    //includeLibrary(exe, "freetype");
-    //b.installBinFile(libs ++ "freetype/freetype.dll", "freetype.dll");
+    linkSDL2(b, exe);
+    linkFreetype(b, exe);
 
     b.installBinFile("deps/Hack Regular Nerd Font Complete.ttf", "Hack Regular Nerd Font Complete.ttf");
 
@@ -45,13 +31,7 @@ pub fn build(b: *std.build.Builder) void {
     run_step.dependOn(&run_cmd.step);
 }
 
-fn includeLibrary(step: *std.build.LibExeObjStep, comptime dir_name: []const u8) void {
-    step.addIncludePath(includes ++ dir_name);
-    step.addLibraryPath(libs ++ dir_name);
-    step.linkSystemLibraryName(dir_name);
-}
-
-fn get_c_sourcefiles(builder: *std.build.Builder, search_dir: []const u8) !std.ArrayList([]const u8) {
+fn get_c_sourcefiles(builder: *Builder, search_dir: []const u8) !std.ArrayList([]const u8) {
     var sources = std.ArrayList([]const u8).init(builder.allocator);
 
     var dir = try std.fs.cwd().openIterableDir(search_dir, .{ .access_sub_paths = true });
@@ -70,3 +50,99 @@ fn get_c_sourcefiles(builder: *std.build.Builder, search_dir: []const u8) !std.A
 
     return sources;
 }
+
+fn linkSDL2(b: *Builder, step: *std.build.LibExeObjStep) void {
+    step.addIncludePath("deps/SDL2/include/SDL2");
+    step.addLibraryPath("deps/SDL2/lib");
+    step.linkSystemLibraryName("SDL2");
+
+    // @Todo: dynamic lib crossplatform support
+    b.installBinFile("deps/SDL2/lib/SDL2.dll", "SDL2.dll");
+}
+
+// Freetype build stuff
+// credit to https://github.com/hexops/mach-freetype/blob/main/build.zig
+const ft_root = "./deps/freetype/freetype";
+const ft_include_path = ft_root ++ "/include";
+
+fn linkFreetype(b: *Builder, step: *std.build.LibExeObjStep) void {
+    const ft_lib = buildFreetype(b, step.build_mode, step.target);
+    step.linkLibrary(ft_lib);
+    step.addIncludePath(ft_include_path);
+
+    std.debug.print("Freetype2 Done\n", .{});
+}
+
+fn buildFreetype(b: *Builder, mode: std.builtin.Mode, target: std.zig.CrossTarget) *std.build.LibExeObjStep {
+    std.debug.print("Building Freetype2...\n", .{});
+
+    const lib = b.addStaticLibrary("freetype", null);
+    lib.defineCMacro("FT2_BUILD_LIBRARY", "1");
+    lib.setBuildMode(mode);
+    lib.setTarget(target);
+    lib.linkLibC();
+    lib.addIncludePath(ft_include_path);
+
+    const target_info = (std.zig.system.NativeTargetInfo.detect(target) catch unreachable).target;
+
+    if (target_info.os.tag == .windows) {
+        lib.addCSourceFile(ft_root ++ "/builds/windows/ftsystem.c", &.{});
+        lib.addCSourceFile(ft_root ++ "/builds/windows/ftdebug.c", &.{});
+    } else {
+        lib.addCSourceFile(ft_root ++ "/src/base/ftsystem.c", &.{});
+        lib.addCSourceFile(ft_root ++ "/src/base/ftdebug.c", &.{});
+    }
+    if (target_info.os.tag.isBSD() or target_info.os.tag == .linux) {
+        lib.defineCMacro("HAVE_UNISTD_H", "1");
+        lib.defineCMacro("HAVE_FCNTL_H", "1");
+        lib.addCSourceFile(ft_root ++ "/builds/unix/ftsystem.c", &.{});
+        if (target_info.os.tag == .macos)
+            lib.addCSourceFile(ft_root ++ "/src/base/ftmac.c", &.{});
+    }
+
+    lib.addCSourceFiles(freetype_base_sources, &.{});
+    return lib;
+}
+
+const freetype_base_sources = &[_][]const u8{
+    ft_root ++ "/src/autofit/autofit.c",
+    ft_root ++ "/src/base/ftbase.c",
+    ft_root ++ "/src/base/ftbbox.c",
+    ft_root ++ "/src/base/ftbdf.c",
+    ft_root ++ "/src/base/ftbitmap.c",
+    ft_root ++ "/src/base/ftcid.c",
+    ft_root ++ "/src/base/ftfstype.c",
+    ft_root ++ "/src/base/ftgasp.c",
+    ft_root ++ "/src/base/ftglyph.c",
+    ft_root ++ "/src/base/ftgxval.c",
+    ft_root ++ "/src/base/ftinit.c",
+    ft_root ++ "/src/base/ftmm.c",
+    ft_root ++ "/src/base/ftotval.c",
+    ft_root ++ "/src/base/ftpatent.c",
+    ft_root ++ "/src/base/ftpfr.c",
+    ft_root ++ "/src/base/ftstroke.c",
+    ft_root ++ "/src/base/ftsynth.c",
+    ft_root ++ "/src/base/fttype1.c",
+    ft_root ++ "/src/base/ftwinfnt.c",
+    ft_root ++ "/src/bdf/bdf.c",
+    ft_root ++ "/src/bzip2/ftbzip2.c",
+    ft_root ++ "/src/cache/ftcache.c",
+    ft_root ++ "/src/cff/cff.c",
+    ft_root ++ "/src/cid/type1cid.c",
+    ft_root ++ "/src/gzip/ftgzip.c",
+    ft_root ++ "/src/lzw/ftlzw.c",
+    ft_root ++ "/src/pcf/pcf.c",
+    ft_root ++ "/src/pfr/pfr.c",
+    ft_root ++ "/src/psaux/psaux.c",
+    ft_root ++ "/src/pshinter/pshinter.c",
+    ft_root ++ "/src/psnames/psnames.c",
+    ft_root ++ "/src/raster/raster.c",
+    ft_root ++ "/src/sdf/sdf.c",
+    ft_root ++ "/src/sfnt/sfnt.c",
+    ft_root ++ "/src/smooth/smooth.c",
+    ft_root ++ "/src/svg/svg.c",
+    ft_root ++ "/src/truetype/truetype.c",
+    ft_root ++ "/src/type1/type1.c",
+    ft_root ++ "/src/type42/type42.c",
+    ft_root ++ "/src/winfonts/winfnt.c",
+};
--- src/main.c
+++ src/main.c
@@ -6,10 +6,14 @@
 
 #define SDL_MAIN_HANDLED
 #include <SDL.h>
-#include <SDL_ttf.h>
+//#include <SDL_ttf.h>
 
-#include "sdl_utils.h"
+#include <ft2build.h>
+#include FT_FREETYPE_H
+
+#include "err_utils.h"
 #include "editor.h"
+#include "free_glyph.h"
 
 #define SDL_COLOR_WHITE (SDL_Color){.r = 255, .g = 255, .b = 255, .a = 255}
 
@@ -35,34 +39,46 @@ char* concat_str(char* s1, char* s2) {
 	return new_str;
 }
 
-void render_text(Editor* e, SDL_Renderer* renderer, TTF_Font* font, SDL_Color color) {
-	if(e->data.count <= 1) {
-		return;
-	}
+// void render_text(Editor* e, SDL_Renderer* renderer, TTF_Font* font, SDL_Color color) {
+// 	if(e->data.count <= 1) {
+// 		return;
+// 	}
 
-	SDL_Rect text_rect = {0};
+// 	SDL_Rect text_rect = {0};
 
-	SDL_Surface* txt_surface = CheckSDLPtr(TTF_RenderUTF8_Blended_Wrapped(font, e->data.items, color, 0));
-	SDL_Texture* txt_texture = CheckSDLPtr(SDL_CreateTextureFromSurface(renderer, txt_surface));
+// 	SDL_Surface* txt_surface = CheckSDLPtr(TTF_RenderUTF8_Blended_Wrapped(font, e->data.items, color, 0));
+// 	SDL_Texture* txt_texture = CheckSDLPtr(SDL_CreateTextureFromSurface(renderer, txt_surface));
 
-	CheckSDLError(SDL_QueryTexture(txt_texture, 0, 0, &text_rect.w, &text_rect.h));
-	CheckSDLError(SDL_RenderCopy(renderer, txt_texture, 0, &text_rect));
-}
+// 	CheckSDLError(SDL_QueryTexture(txt_texture, 0, 0, &text_rect.w, &text_rect.h));
+// 	CheckSDLError(SDL_RenderCopy(renderer, txt_texture, 0, &text_rect));
+// }
 
 int main(int argc, char* argv[]) {
 	printf("Starting Wheel...\n");
-	char* base_path = CheckSDLPtr(SDL_GetBasePath());
 
 	CheckSDLError(SDL_Init(SDL_INIT_VIDEO));
-	CheckSDLError(TTF_Init());
+	// CheckSDLError(TTF_Init());
 
+	char* base_path = CheckSDLPtr(SDL_GetBasePath());
 	//printf("BasePath: %s\n", base_path);
+	// @Todo: sometimes default_font_path has random data in between base_path and font_name
 	char* default_font_path = concat_str(base_path, DEFAULT_FONT_NAME);
-	TTF_Font* font = CheckSDLPtr(TTF_OpenFont(default_font_path, FONT_SIZE));
+	//TTF_Font* font = CheckSDLPtr(TTF_OpenFont(default_font_path, FONT_SIZE));
+
+	FT_Library ft;
+	CheckFTError(FT_Init_FreeType(&ft), "Could't init library");
+
+	FT_Face face;
+	CheckFTError(FT_New_Face(ft, default_font_path, 0, &face), "Could't open font " DEFAULT_FONT_NAME);
 	free(default_font_path);
+
+	FT_Set_Pixel_Sizes(face, 0, FONT_SIZE);
+
 
 	SDL_Window* window = CheckSDLPtr(SDL_CreateWindow("Wheel", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 1280, 720, SDL_WINDOW_RESIZABLE));
 	SDL_Renderer* renderer = CheckSDLPtr(SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED));
+
+	SDL_Texture* atlas = CheckPtr(CreateFontAtlas(renderer, face), "ERROR: Unable to create font atlas!\n");
 
 	// append null terminator so SDL doesn't go kaput
 	LIST_APPEND(Data, &editor.data, '\0');
@@ -103,15 +119,16 @@ int main(int argc, char* argv[]) {
 		CheckSDLError(SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0));
 		CheckSDLError(SDL_RenderClear(renderer));
 		
-		render_text(&editor, renderer, font, SDL_COLOR_WHITE);
+		// render_text(&editor, renderer, font, SDL_COLOR_WHITE);
+		CheckSDLError(SDL_RenderCopy(renderer, atlas, 0, 0));
 
 		SDL_RenderPresent(renderer);
 	}
 
 	free(editor.data.items);
 	
-	TTF_CloseFont(font);
-	TTF_Quit();
+	//TTF_CloseFont(font);
+	//TTF_Quit();
 	SDL_Quit();
 	return 0;
 }
