965f1cd4f4a8b620042a54e7ca44751698243605
Parzival-3141 <29632054+Parzival-3141@users.noreply.github.com>
2023-05-19 02:16:46 -0400

    Update submodules
    Updated cglm and freetype. Freetype now uses the Zig package manager,
    which calls the upstream build.zig and returns the output to be linked.
    Not sure how stable this is, but it works pretty nicely.

--- build.zig
+++ build.zig
@@ -10,10 +10,9 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
     });
     exe.linkLibC();
-    // exe.verbose_cc = true;
     b.installArtifact(exe);
 
-    const csources = get_file_paths(b, "src", ".c") catch unreachable;
+    const csources = get_project_filepaths(b, "src", ".c") catch unreachable;
     defer csources.deinit();
 
     exe.addCSourceFiles(csources.items, &.{ "-Wall", "-Wextra", "-pedantic", "--debug", "-Werror", "-Wno-unused-parameter" });
@@ -30,18 +29,17 @@ pub fn build(b: *std.Build) void {
     exe.step.dependOn(&fs_obj.step);
     exe.addObject(fs_obj);
 
-    linkSDL2(b, exe);
-    linkFreetype(b, exe);
-    link_glad(b, exe);
+    addSDL2(b, exe);
+    addFreetype(b, exe);
+    addGlad(b, exe);
     exe.addIncludePath("deps/cglm/include");
 
     b.installBinFile("assets/Hack Regular Nerd Font Complete.ttf", "assets/Hack Regular Nerd Font Complete.ttf");
 
-    const shaders = get_file_paths(b, "assets/shaders", null) catch unreachable;
+    const shaders = get_project_filepaths(b, "assets/shaders", null) catch unreachable;
     defer shaders.deinit();
 
-    // @Note: Obtained from https://fonts.google.com/icons
-    const icons = get_file_paths(b, "assets/icons", null) catch unreachable;
+    const icons = get_project_filepaths(b, "assets/icons", null) catch unreachable;
     defer icons.deinit();
 
     installBinFiles(b, shaders.items, "assets/shaders") catch unreachable;
@@ -64,7 +62,11 @@ fn installBinFiles(b: *std.Build, src_paths: [][]const u8, dest_relative_path: [
     }
 }
 
-fn get_file_paths(b: *std.Build, search_dir: []const u8, extension: ?[]const u8) !std.ArrayList([]const u8) {
+fn get_project_filepaths(b: *std.Build, sub_dir: []const u8, extension: ?[]const u8) !std.ArrayList([]const u8) {
+    return get_filepaths(b, b.pathFromRoot(sub_dir), extension);
+}
+
+fn get_filepaths(b: *std.Build, search_dir: []const u8, extension: ?[]const u8) !std.ArrayList([]const u8) {
     var paths = std.ArrayList([]const u8).init(b.allocator);
 
     var dir = try std.fs.cwd().openIterableDir(search_dir, .{ .access_sub_paths = true });
@@ -88,7 +90,7 @@ fn get_file_paths(b: *std.Build, search_dir: []const u8, extension: ?[]const u8)
     return paths;
 }
 
-fn linkSDL2(b: *std.Build, step: *std.Build.LibExeObjStep) void {
+fn addSDL2(b: *std.Build, step: *std.Build.LibExeObjStep) void {
     step.addIncludePath("deps/SDL2/include");
     step.addLibraryPath("deps/SDL2");
     step.linkSystemLibrary("SDL2");
@@ -98,107 +100,30 @@ fn linkSDL2(b: *std.Build, step: *std.Build.LibExeObjStep) void {
 }
 
 const glad_path = "deps/glad";
-fn link_glad(b: *std.Build, step: *std.Build.LibExeObjStep) void {
+fn addGlad(b: *std.Build, step: *std.Build.LibExeObjStep) void {
     const glad = build_glad(b, step.optimize, step.target);
     step.linkLibrary(glad);
     step.addIncludePath(glad_path);
 }
 
-fn build_glad(b: *std.Build, mode: std.builtin.Mode, target: std.zig.CrossTarget) *std.Build.LibExeObjStep {
-    const lib = b.addStaticLibrary(.{ .name = "glad", .target = target, .optimize = mode });
+fn build_glad(b: *std.Build, optimize: std.builtin.OptimizeMode, target: std.zig.CrossTarget) *std.Build.LibExeObjStep {
+    const lib = b.addStaticLibrary(.{
+        .name = "glad",
+        .target = target,
+        .optimize = optimize,
+    });
     lib.linkLibC();
-
     lib.addIncludePath(glad_path);
 
-    const glad_src = get_file_paths(b, glad_path, ".c") catch unreachable;
+    const glad_src = get_project_filepaths(b, glad_path, ".c") catch unreachable;
     lib.addCSourceFiles(glad_src.items, &.{});
     glad_src.deinit();
 
     return lib;
 }
 
-// Freetype build stuff
-// credit to https://github.com/hexops/mach-freetype/blob/main/build.zig
-const ft_root = "./deps/freetype/freetype";
-const ft_include_path = ft_root ++ "/include";
-
-fn linkFreetype(b: *std.Build, step: *std.Build.LibExeObjStep) void {
-    const ft_lib = buildFreetype(b, step.optimize, step.target);
-    step.linkLibrary(ft_lib);
-    step.addIncludePath(ft_include_path);
+fn addFreetype(b: *std.Build, step: *std.Build.LibExeObjStep) void {
+    const ft = b.anonymousDependency("deps/freetype", @import("deps/freetype/build.zig"), .{});
+    step.linkLibrary(ft.artifact("freetype"));
+    step.addIncludePath("deps/freetype/include");
 }
-
-fn buildFreetype(b: *std.Build, mode: std.builtin.Mode, target: std.zig.CrossTarget) *std.Build.LibExeObjStep {
-    const lib = b.addStaticLibrary(.{
-        .name = "freetype",
-        .target = target,
-        .optimize = mode,
-    });
-
-    lib.defineCMacro("FT2_BUILD_LIBRARY", "1");
-    lib.linkLibC();
-    lib.addIncludePath(ft_include_path);
-
-    const target_info = (std.zig.system.NativeTargetInfo.detect(target) catch unreachable).target;
-
-    if (target_info.os.tag == .windows) {
-        lib.addCSourceFile(ft_root ++ "/builds/windows/ftsystem.c", &.{});
-        lib.addCSourceFile(ft_root ++ "/builds/windows/ftdebug.c", &.{});
-    } else {
-        lib.addCSourceFile(ft_root ++ "/src/base/ftsystem.c", &.{});
-        lib.addCSourceFile(ft_root ++ "/src/base/ftdebug.c", &.{});
-    }
-    if (target_info.os.tag.isBSD() or target_info.os.tag == .linux) {
-        lib.defineCMacro("HAVE_UNISTD_H", "1");
-        lib.defineCMacro("HAVE_FCNTL_H", "1");
-        lib.addCSourceFile(ft_root ++ "/builds/unix/ftsystem.c", &.{});
-        if (target_info.os.tag == .macos)
-            lib.addCSourceFile(ft_root ++ "/src/base/ftmac.c", &.{});
-    }
-
-    lib.addCSourceFiles(freetype_base_sources, &.{});
-    return lib;
-}
-
-const freetype_base_sources = &[_][]const u8{
-    ft_root ++ "/src/autofit/autofit.c",
-    ft_root ++ "/src/base/ftbase.c",
-    ft_root ++ "/src/base/ftbbox.c",
-    ft_root ++ "/src/base/ftbdf.c",
-    ft_root ++ "/src/base/ftbitmap.c",
-    ft_root ++ "/src/base/ftcid.c",
-    ft_root ++ "/src/base/ftfstype.c",
-    ft_root ++ "/src/base/ftgasp.c",
-    ft_root ++ "/src/base/ftglyph.c",
-    ft_root ++ "/src/base/ftgxval.c",
-    ft_root ++ "/src/base/ftinit.c",
-    ft_root ++ "/src/base/ftmm.c",
-    ft_root ++ "/src/base/ftotval.c",
-    ft_root ++ "/src/base/ftpatent.c",
-    ft_root ++ "/src/base/ftpfr.c",
-    ft_root ++ "/src/base/ftstroke.c",
-    ft_root ++ "/src/base/ftsynth.c",
-    ft_root ++ "/src/base/fttype1.c",
-    ft_root ++ "/src/base/ftwinfnt.c",
-    ft_root ++ "/src/bdf/bdf.c",
-    ft_root ++ "/src/bzip2/ftbzip2.c",
-    ft_root ++ "/src/cache/ftcache.c",
-    ft_root ++ "/src/cff/cff.c",
-    ft_root ++ "/src/cid/type1cid.c",
-    ft_root ++ "/src/gzip/ftgzip.c",
-    ft_root ++ "/src/lzw/ftlzw.c",
-    ft_root ++ "/src/pcf/pcf.c",
-    ft_root ++ "/src/pfr/pfr.c",
-    ft_root ++ "/src/psaux/psaux.c",
-    ft_root ++ "/src/pshinter/pshinter.c",
-    ft_root ++ "/src/psnames/psnames.c",
-    ft_root ++ "/src/raster/raster.c",
-    ft_root ++ "/src/sdf/sdf.c",
-    ft_root ++ "/src/sfnt/sfnt.c",
-    ft_root ++ "/src/smooth/smooth.c",
-    ft_root ++ "/src/svg/svg.c",
-    ft_root ++ "/src/truetype/truetype.c",
-    ft_root ++ "/src/type1/type1.c",
-    ft_root ++ "/src/type42/type42.c",
-    ft_root ++ "/src/winfonts/winfnt.c",
-};
